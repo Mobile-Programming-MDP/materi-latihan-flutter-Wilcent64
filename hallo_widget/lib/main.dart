617a682de826ac15a851ddccda3150eea159d97f
Mahaaraztu <Wilcent_2226250120@mhs.mdp.ac.id>
2023-10-13 18:24:25 +0700

    Pisah Widget

--- hallo_widget/lib/main.dart
+++ hallo_widget/lib/main.dart
@@ -3,21 +3,31 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    title : "Hello Widget",
-    theme : ThemeData(primarySwatch: Colors.blue),
-    home : Scaffold(
-      appBar : AppBar(
-      title: const Text ('Wilcent'),
-    ), //Appbar
-    body: const Center(
-      child: Text("Hello Wilcent"),
-    ),
-    ));
+        title: "Hello Widget",
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: FirstScreen());
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Wilcent'),
+      ), //Appbar
+      body: const Center(
+        child: Text("Hello Wilcent"),
+      ),
+    );
   }
 }
